03f0046fa7e4b2f458b5cf08cb66a2e01db20687
Marcel Marques <marcmatiasdev@gmail.com>
2022-09-21 19:21:30 -0300

    eslint and prettier

--- src/auto-complete.js
+++ src/auto-complete.js
@@ -3,249 +3,249 @@ import nunjucks from "nunjucks";
 import { EventListener } from "./events.js";
 
 function jsonToBase64(data) {
-    return btoa(JSON.stringify(data));
+  return window.btoa(JSON.stringify(data));
 }
 function base64ToJson(text) {
-    return JSON.parse(atob(text));
+  return JSON.parse(window.atob(text));
 }
 
 export class AutoComplete {
-    constructor(selector, {search = undefined, renderItem = undefined, onSelect = undefined, autoFocus = true, minLength = 3, delay = 500} = {}) {
-        let self = this;
-        this._timeout = undefined;
-        this._controlKeys = ["Escape", "ArrowDown", "ArrowUp", "Enter"];
-        this._options = { search, renderItem, onSelect, autoFocus, minLength, delay };
-        this._options.renderItem = this._options.renderItem || this.renderItem;
-        this._template = this.getTemplate();
-        this._container = (
-            typeof(selector) === "string" ?
-            document.querySelector(selector) :
-            selector
-        );
-        this._inputContainer = this._container.querySelector("input[type=search]");
-        this._inputContainer.autocomplete = "off";
-        this._inputEvents = new EventListener(this._container);
-        this._attachInputEventListeners();
-        if (this._options.autoFocus) {
-            this._inputContainer.focus();
-        }
-
-        this._init();
+  constructor(
+    selector,
+    {
+      search = undefined,
+      renderItem = undefined,
+      onSelect = undefined,
+      autoFocus = true,
+      minLength = 3,
+      delay = 500,
+    } = {}
+  ) {
+    this._timeout = undefined;
+    this._controlKeys = ["Escape", "ArrowDown", "ArrowUp", "Enter"];
+    this._options = {
+      search,
+      renderItem,
+      onSelect,
+      autoFocus,
+      minLength,
+      delay,
+    };
+    this._options.renderItem = this._options.renderItem || this.renderItem;
+    this._template = this.getTemplate();
+    this._container =
+      typeof selector === "string"
+        ? document.querySelector(selector)
+        : selector;
+    this._inputContainer = this._container.querySelector("input[type=search]");
+    this._inputContainer.autocomplete = "off";
+    this._inputEvents = new EventListener(this._container);
+    this._attachInputEventListeners();
+    if (this._options.autoFocus) {
+      this._inputContainer.focus();
     }
 
-    _init() {
-        // Showing clear field button any browser
+    this._init();
+  }
 
-    }
+  _init() {
+    // Showing clear field button any browser
+  }
 
-    conditionallyHideClearIcon(e) {
-        const input = this._inputContainer;
+  conditionallyHideClearIcon(e) {
+    const input = this._inputContainer;
 
-        const target = (e && e.target) || input;
-        target.parentElement.querySelector("[data-clear-input]").style.display = target.value ? 'inline' : 'none';
-    }
+    const target = (e && e.target) || input;
+    target.parentElement.querySelector("[data-clear-input]").style.display =
+      target.value ? "inline" : "none";
+  }
 
-    _attachInputEventListeners() {
-        let self = this;
+  _attachInputEventListeners() {
+    let self = this;
 
-        this._inputEvents.attach(
-            "click",
-            "[data-clear-input]",
-            async function(event) {
-                self._inputContainer.value = '';
-                self.conditionallyHideClearIcon();
-            }
-        );
-        this._inputEvents.attach(
-            "input",
-            "input[type=search]",
-            async function(event) {
-                self.conditionallyHideClearIcon(event);
-            }
-        );
-        this._inputEvents.attach(
-            "keydown",
-            "input[type=search]",
-            async function(event) {
-                await self.onKeyDown(event);
-            }
-        );
-        this._inputEvents.attach(
-            "keyup",
-            "input[type=search]",
-            async function(event) {
-                await self.onKeyUp(event);
-            }
-        );
-        this._inputEvents.attach(
-            "click",
-            "span[type=submit]",
-            async function (event, closest) {
-                event.preventDefault();
-                event.stopPropagation();
-                if (self._resultContainer) {
-                    const select = self._resultContainer.querySelector("li");
-                    if (select) {
-                        await self.onSelect(event, select);
-                    }
-                }
-                else {
-                    await self.search();
-                }
-            }
-        );
-        this._inputEvents.attach(
-            "submit",
-            "form",
-            async function (event, closest) {
-                event.preventDefault();
-                event.stopPropagation();
-                if (self._resultContainer) {
-                    const select = self._resultContainer.querySelector("li");
-                    if (select) {
-                        await self.onSelect(event, select);
-                    }
-                }
-                else {
-                    await self.search();
-                }
-            }
-        );
-    }
-
-    _attachResultEventListeners() {
-        let self = this;
-
-        window.addEventListener("click", function (event) {
-            self.close(false);
-        });
-        this._resultEvents.attach(
-            "click",
-            "ul > li.autocomplete-item-none",
-            async function (event, closest) {
-                self.close();
-            }
-        );
-        this._resultEvents.attach(
-            "mouseover",
-            "ul > li.autocomplete-item",
-            async function (event, closest) {
-                let selected = self._resultContainer.querySelector("li.selected");
-                if (selected) {
-                    selected.classList.remove("selected");
-                }
-                closest.classList.add("selected");
-            }
-        );
-        this._resultEvents.attach(
-            "click",
-            "ul > li.autocomplete-item",
-            async function (event, closest) {
-                await self.onSelect(event, closest);
-            }
-        );
-    }
-
-    async onSelect(event, closest) {
-        let item = (
-            closest.classList.contains("autocomplete-item-none") ?
-            null :
-            base64ToJson(closest.dataset.item)
-        );
-        try {
-            await this._options.onSelect(item);
-        }
-        catch (error) {
-            // do nothing
-        }
-        this.close();
-    }
-
-    async onKeyDown(event) {
-        let self = this,
-            target = event.target,
-            key = event.key;
-        if (!self._controlKeys.includes(key)) {
-            return;
-        }
-
+    this._inputEvents.attach("click", "[data-clear-input]", async function () {
+      self._inputContainer.value = "";
+      self.conditionallyHideClearIcon();
+    });
+    this._inputEvents.attach(
+      "input",
+      "input[type=search]",
+      async function (event) {
+        self.conditionallyHideClearIcon(event);
+      }
+    );
+    this._inputEvents.attach(
+      "keydown",
+      "input[type=search]",
+      async function (event) {
+        await self.onKeyDown(event);
+      }
+    );
+    this._inputEvents.attach(
+      "keyup",
+      "input[type=search]",
+      async function (event) {
+        await self.onKeyUp(event);
+      }
+    );
+    this._inputEvents.attach(
+      "click",
+      "span[type=submit]",
+      async function (event) {
         event.preventDefault();
-        if (key == "Escape") {
-            self.close(false);
+        event.stopPropagation();
+        if (self._resultContainer) {
+          const select = self._resultContainer.querySelector("li");
+          if (select) {
+            await self.onSelect(event, select);
+          }
+        } else {
+          await self.search();
         }
-        else if ((key == "ArrowDown" || key == "ArrowUp") && this._resultContainer) {
-            let selected = this._resultContainer.querySelector("li.selected"),
-                nextSelected;
+      }
+    );
+    this._inputEvents.attach("submit", "form", async function (event) {
+      event.preventDefault();
+      event.stopPropagation();
+      if (self._resultContainer) {
+        const select = self._resultContainer.querySelector("li");
+        if (select) {
+          await self.onSelect(event, select);
+        }
+      } else {
+        await self.search();
+      }
+    });
+  }
 
-            if (!selected) {
-                if (key == "ArrowDown") {
-                    nextSelected = this._resultContainer.querySelector("li");
-                }
-                else {
-                    nextSelected = this._resultContainer.querySelectorAll("li:last-child")[0];
-                }
-            }
-            else {
-                nextSelected = (
-                    key == "ArrowDown" ?
-                    selected.nextElementSibling :
-                    selected.previousElementSibling
-                );
-                selected.classList.remove("selected");
-            }
-            if (nextSelected) {
-                nextSelected.classList.add("selected");
-            }
+  _attachResultEventListeners() {
+    let self = this;
+
+    window.addEventListener("click", function () {
+      self.close(false);
+    });
+    this._resultEvents.attach(
+      "click",
+      "ul > li.autocomplete-item-none",
+      async function () {
+        self.close();
+      }
+    );
+    this._resultEvents.attach(
+      "mouseover",
+      "ul > li.autocomplete-item",
+      async function (event, closest) {
+        let selected = self._resultContainer.querySelector("li.selected");
+        if (selected) {
+          selected.classList.remove("selected");
         }
-        else if (key == "Enter") {
-            const self = this,
-                  resContainer = this._resultContainer;
-            if (resContainer) {
-                let select = resContainer.querySelector("li.selected") || resContainer.querySelector("li");
-                if (select) {
-                    await self.onSelect(event, select);
-                }
-            }
-        }
+        closest.classList.add("selected");
+      }
+    );
+    this._resultEvents.attach(
+      "click",
+      "ul > li.autocomplete-item",
+      async function (event, closest) {
+        await self.onSelect(event, closest);
+      }
+    );
+  }
+
+  async onSelect(event, closest) {
+    let item = closest.classList.contains("autocomplete-item-none")
+      ? null
+      : base64ToJson(closest.dataset.item);
+    try {
+      await this._options.onSelect(item);
+    } catch (error) {
+      // do nothing
+    }
+    this.close();
+  }
+
+  async onKeyDown(event) {
+    let self = this,
+      key = event.key;
+    if (!self._controlKeys.includes(key)) {
+      return;
     }
 
-    async onKeyUp(event) {
-        let self = this,
-            target = event.target,
-            key = event.key;
+    event.preventDefault();
+    if (key == "Escape") {
+      self.close(false);
+    } else if (
+      (key == "ArrowDown" || key == "ArrowUp") &&
+      this._resultContainer
+    ) {
+      let selected = this._resultContainer.querySelector("li.selected"),
+        nextSelected;
 
-        if (self._controlKeys.includes(key)) {
-            event.preventDefault();
-            return;
+      if (!selected) {
+        if (key == "ArrowDown") {
+          nextSelected = this._resultContainer.querySelector("li");
+        } else {
+          nextSelected =
+            this._resultContainer.querySelectorAll("li:last-child")[0];
         }
-        else if (
-          target.tagName != "INPUT"
-          || target.value.length < this._options.minLength
-        ) {
-            this.close(false);
-            return;
+      } else {
+        nextSelected =
+          key == "ArrowDown"
+            ? selected.nextElementSibling
+            : selected.previousElementSibling;
+        selected.classList.remove("selected");
+      }
+      if (nextSelected) {
+        nextSelected.classList.add("selected");
+      }
+    } else if (key == "Enter") {
+      const self = this,
+        resContainer = this._resultContainer;
+      if (resContainer) {
+        let select =
+          resContainer.querySelector("li.selected") ||
+          resContainer.querySelector("li");
+        if (select) {
+          await self.onSelect(event, select);
         }
+      }
+    }
+  }
 
-        if (this._timeout) {
-            clearTimeout(this._timeout);
-        }
-        this._timeout = setTimeout(
-            async function() { await self.search(); },
-            this._options.delay,
-        );
+  async onKeyUp(event) {
+    let self = this,
+      target = event.target,
+      key = event.key;
+
+    if (self._controlKeys.includes(key)) {
+      event.preventDefault();
+      return;
+    } else if (
+      target.tagName != "INPUT" ||
+      target.value.length < this._options.minLength
+    ) {
+      this.close(false);
+      return;
     }
 
-    _createResultElement(html, inputContainer) {
-        let div = document.createElement("div");
-        div.classList = "autocomplete-container";
-        div.style.top = inputContainer.offsetTop + inputContainer.offsetHeight;
-        div.style.left = inputContainer.offsetLeft + inputContainer.offsetWidth;
-        div.innerHTML = html;
-        return div;
+    if (this._timeout) {
+      clearTimeout(this._timeout);
     }
+    this._timeout = setTimeout(async function () {
+      await self.search();
+    }, this._options.delay);
+  }
 
-    getTemplate() {
-        let template = `
+  _createResultElement(html, inputContainer) {
+    let div = document.createElement("div");
+    div.classList = "autocomplete-container";
+    div.style.top = inputContainer.offsetTop + inputContainer.offsetHeight;
+    div.style.left = inputContainer.offsetLeft + inputContainer.offsetWidth;
+    div.innerHTML = html;
+    return div;
+  }
+
+  getTemplate() {
+    let template = `
           <ul class="autocomplete-result">
           {% for item in results %}
           <li class="autocomplete-item" data-item="{{ item|jsonToBase64 }}">
@@ -256,43 +256,42 @@ export class AutoComplete {
           {% endfor %}
           </ul>
         `;
-        let env = new nunjucks.Environment();
-        env.addFilter("renderItem", this._options.renderItem, false);
-        env.addFilter("jsonToBase64", jsonToBase64, false);
-        return nunjucks.compile(template, env);
-    }
+    let env = new nunjucks.Environment();
+    env.addFilter("renderItem", this._options.renderItem, false);
+    env.addFilter("jsonToBase64", jsonToBase64, false);
+    return nunjucks.compile(template, env);
+  }
 
-    renderItem(item) {
-        return item.label;
-    }
+  renderItem(item) {
+    return item.label;
+  }
 
-    async search() {
-        let inputContainer = this._inputContainer,
-            results = await this._options.search(inputContainer.value),
-            html = this._template.render({ results });
+  async search() {
+    let inputContainer = this._inputContainer,
+      results = await this._options.search(inputContainer.value),
+      html = this._template.render({ results });
 
-        if (this._resultContainer) {
-            this._resultContainer.innerHTML = html;
-        }
-        else {
-            let div = this._createResultElement(html, inputContainer);
-            this._container.appendChild(div);
-            this._resultContainer = div;
-            this._resultEvents = new EventListener(div.parentElement);
-            this._attachResultEventListeners();
-        }
+    if (this._resultContainer) {
+      this._resultContainer.innerHTML = html;
+    } else {
+      let div = this._createResultElement(html, inputContainer);
+      this._container.appendChild(div);
+      this._resultContainer = div;
+      this._resultEvents = new EventListener(div.parentElement);
+      this._attachResultEventListeners();
     }
+  }
 
-    close(clear=true) {
-        if (this._resultContainer) {
-            this._resultContainer.remove();
-            this._resultEvents.detachAll();
-            this._resultContainer = undefined;
-            this._resultEvents = undefined;
-        }
-        if (clear) {
-            this._inputContainer.value = "";
-            this.conditionallyHideClearIcon();
-        }
+  close(clear = true) {
+    if (this._resultContainer) {
+      this._resultContainer.remove();
+      this._resultEvents.detachAll();
+      this._resultContainer = undefined;
+      this._resultEvents = undefined;
     }
+    if (clear) {
+      this._inputContainer.value = "";
+      this.conditionallyHideClearIcon();
+    }
+  }
 }
--- src/events.js
+++ src/events.js
@@ -4,62 +4,63 @@
  * so memory is saved.
  */
 export class EventListener {
+  constructor(selectorOrElement) {
+    let self = this;
+    this.container =
+      typeof selectorOrElement === "string"
+        ? document.querySelector(selectorOrElement)
+        : selectorOrElement;
+    this._listeners = {};
+    this._callback = async function (event) {
+      await self._handle(event);
+    };
+  }
 
-    constructor(selectorOrElement) {
-        let self = this;
-        this.container = (
-          typeof(selectorOrElement) === "string" ?
-          document.querySelector(selectorOrElement) :
-          selectorOrElement
-        );
-        this._listeners = {};
-        this._callback = async function(event) {
-            await self._handle(event);
-        };
+  detach(eventType, selector) {
+    delete this._listeners[eventType][selector];
+    if (Object.keys(this._listeners[eventType]).length == 0) {
+      // No more attached events of this type, so remove listener
+      this.container.removeEventListener(eventType, this._callback);
     }
+  }
 
-    detach(eventType, selector) {
-        delete this._listeners[eventType][selector];
-        if (Object.keys(this._listeners[eventType]).length == 0) {
-            // No more attached events of this type, so remove listener
-            this.container.removeEventListener(eventType, this._callback);
+  detachAll() {
+    for (let eventType of Object.keys(this._listeners)) {
+      for (let [, callbacks] of Object.entries(this._listeners[eventType])) {
+        for (let i = 0; i < callbacks.length; i++) {
+          this.container.removeEventListener(eventType, this._callback);
         }
+      }
     }
+    this._listeners = {};
+  }
 
-    detachAll() {
-        for (let eventType of Object.keys(this._listeners)) {
-            for (let [selector, callbacks] of Object.entries(this._listeners[eventType])) {
-                for (let callback of callbacks) {
-                    this.container.removeEventListener(eventType, this._callback);
-                }
-            }
-        }
-        this._listeners = {};
+  attach(eventType, selector, callback) {
+    if (this._listeners[eventType] === undefined) {
+      // First time seeing this event on this container, so attach
+      // listener.
+      this._listeners[eventType] = {};
+      this.container.addEventListener(eventType, this._callback);
     }
-
-    attach(eventType, selector, callback) {
-        if (this._listeners[eventType] === undefined) {
-            // First time seeing this event on this container, so attach
-            // listener.
-            this._listeners[eventType] = {};
-            this.container.addEventListener(eventType, this._callback);
-        }
-        if (this._listeners[eventType][selector] === undefined) {
-            this._listeners[eventType][selector] = [];
-        }
-        this._listeners[eventType][selector].push(callback);
+    if (this._listeners[eventType][selector] === undefined) {
+      this._listeners[eventType][selector] = [];
     }
+    this._listeners[eventType][selector].push(callback);
+  }
 
-    async _handle(event) {
-        let target = event.target, eventType = event.type;
+  async _handle(event) {
+    let target = event.target,
+      eventType = event.type;
 
-        for (let [selector, callbacks] of Object.entries(this._listeners[eventType])) {
-            let closest = target.closest(selector);
-            if (closest && closest.disabled !== true) {
-                for (let callback of callbacks) {
-                    await Promise.resolve(callback(event, closest));
-                }
-            }
+    for (let [selector, callbacks] of Object.entries(
+      this._listeners[eventType]
+    )) {
+      let closest = target.closest(selector);
+      if (closest && closest.disabled !== true) {
+        for (let callback of callbacks) {
+          await Promise.resolve(callback(event, closest));
         }
+      }
     }
+  }
 }
